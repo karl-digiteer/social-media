3fb40d18a0c3402aa393ba646359ab4e354a9497
Karl Asares <asareskarl.digiteer@gmail.com>
2025-05-08 17:22:23 +0800

    Added users and customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :customers
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get 'feed', to: 'home#feed'
